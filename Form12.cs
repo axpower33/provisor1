cdfb3f1389aa52159de4f3d513fcf9f162983ef0
axpower33 <95838463+axpower33@users.noreply.github.com>
2023-09-30 10:29:12 +0700

    Add files via upload

--- Form12.cs
+++ Form12.cs
@@ -26,24 +26,62 @@ namespace WindowsFormsApp8
 
         private void Form12_Load(object sender, EventArgs e)
         {
+            DateTime fromDate2 = new DateTime(2010, 05, 03);
             // TODO: данная строка кода позволяет загрузить данные в таблицу "ProvisorBaseDataDataSet5.DataTable1". При необходимости она может быть перемещена или удалена.
-            string _selectQuery = @"SELECT        Nomenklatura, SUM(NachOst) AS NachOst, SUM(Prihod) AS Prihod, SUM(Rashod) AS Rashod, SUM(KonOst) AS KonOst" +
-" FROM            (SELECT DISTINCT Nomenklatura.Naimenovanie AS Nomenklatura, SUM(0.00) AS NachOst, SUM(TableTableChast.Summa) AS Prihod, SUM(0.00) AS Rashod, SUM(TableTableChast.Summa - 0.00) AS KonOst" +
-                          " FROM TableTableChast LEFT OUTER JOIN" +
+            string _selectQuery = @"SELECT    Nomenklatura, SUM(NachOst) AS NachOst, SUM(Prihod) AS Prihod, SUM(Rashod) AS Rashod, SUM(KonOst) AS KonOst" +
+" FROM            (SELECT   Nomenklatura.Naimenovanie AS Nomenklatura, SUM(0.00) AS NachOst, SUM(TableTableChast.Summa) AS Prihod, SUM(0.00) AS Rashod, SUM(TableTableChast.Summa - 0.00) AS KonOst" +
+                          " FROM            TableTableChast LEFT OUTER JOIN" +
                           " Nomenklatura AS Nomenklatura ON TableTableChast.Nomenklatura = Nomenklatura.Id" +
+                          " left outer join TableDoc on TableDoc.Id=TableTableChast.Id"+
+                          " WHERE (TableDoc.DataDoc BETWEEN '" + fromDate.Value + "' AND '" + toDate.Value + "') " +
                           " GROUP BY Nomenklatura.Naimenovanie" +
                           " UNION" +
-                          " SELECT DISTINCT Nomenklatura.Naimenovanie AS Nomenklatura, SUM(0.00) AS NachOst, SUM(0.00) AS Prihod, SUM(D.Summa) AS Rashod, SUM(0.00 - D.Summa) AS KonOst" +
-                          " FROM TableTableChast2 AS D LEFT OUTER JOIN" +
+                          " SELECT  Nomenklatura.Naimenovanie AS Nomenklatura, SUM(0.00) AS NachOst, SUM(0.00) AS Prihod, SUM(D.Summa) AS Rashod, SUM(0.00 - D.Summa) AS KonOst" +
+                          " FROM            TableTableChast2 AS D LEFT OUTER JOIN" +
                           " Nomenklatura AS Nomenklatura ON D.Nomenklatura = Nomenklatura.Id" +
-                          " GROUP BY Nomenklatura.Naimenovanie) AS derivedtbl_1"+
+                          " left outer join TableDoc2 on TableDoc2.Id=D.Id" +
+                          " WHERE (TableDoc2.DataDoc BETWEEN '" + fromDate.Value + "' AND '" + toDate.Value + "') " +
+                          " GROUP BY Nomenklatura.Naimenovanie) AS derivedtbl_1" +
+" GROUP BY Nomenklatura";
+            string _selectQuery2 = @"SELECT     Nomenklatura, SUM(NachOst) AS NachOst, Sum(0.00) AS Prihod, SUM(0.00) AS Rashod, SUM(0.00) AS KonOst" +
+" FROM            (SELECT   Nomenklatura.Naimenovanie AS Nomenklatura, SUM(TableTableChast.Summa) AS NachOst, SUM(0.00) AS Prihod, SUM(0.00) AS Rashod, SUM(0.00) AS KonOst" +
+                          " FROM            TableTableChast LEFT OUTER JOIN" +
+                          " Nomenklatura AS Nomenklatura ON TableTableChast.Nomenklatura = Nomenklatura.Id" +
+                          " left outer join TableDoc on TableDoc.Id=TableTableChast.Id" +
+                          " WHERE (TableDoc.DataDoc BETWEEN '" + fromDate2 + "' AND '" + fromDate.Value + "') " +
+                          " GROUP BY Nomenklatura.Naimenovanie" +
+                          " UNION" +
+                          " SELECT  Nomenklatura.Naimenovanie AS Nomenklatura, SUM(0.00) AS NachOst, SUM(0.00) AS Prihod, SUM(D.Summa) AS Rashod, SUM(0.00 - D.Summa) AS KonOst" +
+                          " FROM            TableTableChast2 AS D LEFT OUTER JOIN" +
+                          " Nomenklatura AS Nomenklatura ON D.Nomenklatura = Nomenklatura.Id" +
+                          " left outer join TableDoc2 on TableDoc2.Id=D.Id" +
+                          " WHERE (TableDoc2.DataDoc BETWEEN '" + fromDate2 + "' AND '" + fromDate.Value + "') " +
+                          " GROUP BY Nomenklatura.Naimenovanie) AS derivedtbl_2" +
 " GROUP BY Nomenklatura";
 
             IDbConnection db;
             db = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = ""C:\Users\Basko\SqlBases\ProvisorBaseData.mdf""; Integrated Security = True; Connect Timeout = 20");
             db.Open();
 
-            List<DataTable4> dt = db.Query<DataTable4>(_selectQuery).AsList<DataTable4>().ToList();
+            List<DataTable4> dt = db.Query<DataTable4>(_selectQuery).AsList<DataTable4>();
+            List<DataTable4> dt2 = db.Query<DataTable4>(_selectQuery2).AsList<DataTable4>();
+            int i = 0;
+            int j;
+
+            while (i<dt.Count())
+            {
+                j = 0;
+                while (j < dt2.Count())
+                {
+                    if (dt[i].Nomenklatura.ToString() == dt2[j].Nomenklatura.ToString())
+                    {
+                        dt[i].NachOst = dt2[j].NachOst;
+                        dt[i].KonOst = dt[i].NachOst + dt[i].Prihod - dt[i].Rashod;
+                    }
+                    j++;
+                }
+                i++;
+            }
             db.Close();
 
             this.DataTable1BindingSource.DataSource = dt;
